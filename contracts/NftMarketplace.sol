75de78a61a9d3225bef774f1aae3ea1fca199779
Sami-Shafi <freelancersami123@gmail.com>
2023-01-09 05:33:28 +0600

    fixed bug

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -12,58 +12,73 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedOnMarketPlace();
 error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
-error NftMarketplace__AlreadyListed();
+error NftMarketplace__NotOwner();
 
 contract NftMarketplace {
-
     // *** Enums / Types *** //
 
-    enum Listing {
+    struct Listing {
         uint256 price;
         address seller;
     }
 
-
     // *** Events *** //
 
     event ItemListed(
-        address indexed seller;
-        address indexed nftAddress;
-        address indexed tokenId;
-        uint256 price;
-    )
-
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
 
     // *** Modifiers *** //
 
-    modifier notListed(address nftAddress, uint256 tokenId, address owner) {
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
         Listing memory listing = s_listings[nftAddress][tokenId];
-        if(listing.price > 0) revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        if (listing.price > 0)
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
         _;
     }
 
-    modifier isOwner(address nftAddress, uint256 tokenId, address spender) {
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
         IERC721 nft = IERC721(nftAddress);
         address owner = nft.ownerOf(tokenId);
-        if(spender != owner) revert NftMarketplace__NotOwner();
+        if (spender != owner) revert NftMarketplace__NotOwner();
         _;
     }
-
 
     // *** State Variables *** //
 
+    // *** Mappings *** //
+
     // NFT Contract Address => NFT TokenID -> Listing
-    mapping (address => mapping (uint256 => Listing)) private s_listings;
-    
-    
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
     // *** Main Functions *** //
-    
-    function listItem(address nftAddress, uint256 tokenId, uint256 price) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
-        if(price <= 0) revert NftMarketplace__PriceMustBeAboveZero();
+
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) revert NftMarketplace__PriceMustBeAboveZero();
         IERC721 nft = IERC721(nftAddress);
 
         // if the user doesn't approve the marketplace to hold NFTs
-        if(nft.getApproved(tokenId) != address(this)) revert NftMarketplace__NotApprovedOnMarketPlace();
+        if (nft.getApproved(tokenId) != address(this))
+            revert NftMarketplace__NotApprovedOnMarketPlace();
 
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
