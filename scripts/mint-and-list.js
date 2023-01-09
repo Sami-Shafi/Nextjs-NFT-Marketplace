49d6a85b5f8f69b3774ab8e31dfa95414e026b84
Sami-Shafi <freelancersami123@gmail.com>
2023-01-10 03:17:55 +0600

    some extra scripts

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,5 +1,5 @@
 const { ethers, network } = require("hardhat");
-// const { moveBlocks } = require("../utils/move-blocks")
+const { moveBlocks } = require("../utils/move-blocks");
 
 const PRICE = ethers.utils.parseEther("0.1");
 
@@ -14,7 +14,7 @@ async function mintAndList() {
 	// } else {
 	//     basicNft = await ethers.getContract("BasicNft")
 	// }
-    
+
 	console.log("Minting NFT...");
 	const mintTx = await basicNft.mintNft();
 	const mintTxReceipt = await mintTx.wait(1);
@@ -28,6 +28,8 @@ async function mintAndList() {
 	const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE);
 	await tx.wait(1);
 	console.log("NFT Listed!");
+
+    // moveBlocks for moralis, not needed if you don't use moralis for backend
 	// if (network.config.chainId == 31337) {
 	// 	// Moralis has a hard time if you move more than 1 at once!
 	// 	await moveBlocks(1, (sleepAmount = 1000));
