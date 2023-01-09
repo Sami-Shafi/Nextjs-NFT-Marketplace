dc770ac961993b3e13dc3f4590d04224a2f7485c
Sami-Shafi <freelancersami123@gmail.com>
2023-01-10 01:53:41 +0600

    basic codes

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -4,10 +4,17 @@ import { ConnectButton } from "web3uikit";
 export default function Header() {
 	return (
 		<>
-			<nav>
-				<Link href="/">Home</Link>
-				<Link href="/sell-nft">Sell NFT</Link>
-				<ConnectButton />
+			<nav className="p-5 border-b-2 flex flex-row jusitfy-between items-center w-100">
+				<h1 className="py-4 px-4 font-bold text-3xl">NFT SEA</h1>
+				<div className="flex flex-row items-center ml-auto">
+					<Link href="/" className="mr-4 p-6">
+						Home
+					</Link>
+					<Link href="/sell-nft" className="mr-4 p-6">
+						Sell NFT
+					</Link>
+					<ConnectButton moralisAuth={false} />
+				</div>
 			</nav>
 		</>
 	);
